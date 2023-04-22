789569062abd130fb09090f108900d0a746f0af2
rajesh <rajesh2012y@gmail.com>
2023-04-22 06:05:28 +0000

    complete-code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css'
 const App = () => (
   <>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/team-matches/:id" component={TeamMatches} />
       <Route component={NotFound} />
     </Switch>
@@ -17,3 +17,25 @@ const App = () => (
 )
 
 export default App
+
+// import {Route, Switch} from 'react-router-dom'
+
+// import Home from './components/Home'
+// import NotFound from './components/NotFound'
+// import TeamMatches from './components/TeamMatches'
+
+// import './App.css'
+
+// const App = () => (
+//   // FIX2: Routes should be wrapped with Switch component from react-router-dom
+//   <Switch>
+//     {/* FIX3: "exact" keyword should be added */}
+//     <Route exact path="/" component={Home} />
+//     {/* FIX4: The Route component should be given the prop "component" */}
+//     {/* FIX5:  When mentioning path parameters for a route we need to use ":" before the variable */}
+//     <Route path="/team-matches/:id" component={TeamMatches} />
+//     <Route component={NotFound} />
+//   </Switch>
+// )
+
+// export default App
